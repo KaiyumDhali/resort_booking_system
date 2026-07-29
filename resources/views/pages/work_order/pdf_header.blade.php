d68c17ee7515005bcd7342a3592973efdc2db30e
Kaiyum Dhali <ahmedkaium1998@gmail.com>
2026-07-29 13:03:23 +0600

    Fix work order render issue

--- resources/views/pages/work_order/pdf_header.blade.php
+++ resources/views/pages/work_order/pdf_header.blade.php
@@ -1,31 +1,31 @@
 @php
-    $imagePath = !empty($data['company_logo_one'])
-        ? public_path(\Illuminate\Support\Facades\Storage::url($data['company_logo_one']))
-        : '';
+    use Illuminate\Support\Facades\Storage;
 
-    $imageDataUri = '';
+    $logoPath = '';
 
-    if (!empty($data['company_logo_one']) && file_exists($imagePath)) {
-        $imageDataUri = 'data:' . mime_content_type($imagePath)
-            . ';base64,' . base64_encode(file_get_contents($imagePath));
+    if (!empty($data['company_logo_one'])) {
+        $logoPath = public_path(Storage::url($data['company_logo_one']));
     }
 @endphp
 
 <table style="width:100%; border:none; border-collapse:collapse; font-family:'Kalpurush', sans-serif;">
     <tr>
         <td style="width:20%; vertical-align:middle; border:none;">
-            @if($imageDataUri)
-                <img src="{{ $imageDataUri }}" style="height:60px;" alt="logo">
+            @if(!empty($logoPath) && file_exists($logoPath))
+                <img src="{{ $logoPath }}" style="height:60px;" alt="Company Logo">
             @endif
         </td>
+
         <td style="width:80%; text-align:center; vertical-align:middle; border:none;">
             <div style="font-size:28px; font-weight:bold; font-style:italic; color:#015892; text-transform:uppercase;">
                 {{ $data['company_name'] }}
             </div>
-            <div style="font-size:24px; font-weight:bold; color:#46AA4D; text-transform:uppercase;">
-                ওয়ান্ডার পার্ক এন্ড ইকো রিসোর্ট
+
+            <div style="font-size:24px; font-weight:bold; color:#46AA4D;">
+                আপন ভুবন পিকনিক এবং শুটিং স্পট
             </div>
         </td>
     </tr>
 </table>
-<hr>
+
+<hr style="border:0;border-top:1px solid #000;margin-top:5px;">
